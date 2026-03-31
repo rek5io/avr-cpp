37bfa3b65fd5f6062a4ce6de09f51b0491ac4a40
rek5io <janjuskowiak2004@gmail.com>
2026-03-31 15:54:14 +0200

    feat: add Timer1

--- test/src/avr_io.hpp
+++ test/src/avr_io.hpp
@@ -2,112 +2,240 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
-namespace io {
-    namespace regs {
-        struct RegB {
-            inline static volatile uint8_t& ddr() {
-                return DDRB;
-            }
+namespace atmega_328p {
+    namespace timers {
+        enum TimeUnit : uint8_t {
+            Micro = 0,
+            Sec = 1,
+            Hour = 2
+        }; 
 
-            inline static volatile uint8_t& port() {
-                return PORTB;
-            }
+        struct Timer1 {
+            static void (*callback)();
 
-            inline static volatile uint8_t& pin() {
-                return PINB;
+            static void init(TimeUnit unit, uint32_t duration, void (*cb)()) {
+                cli();
+
+                callback = cb;
+
+                if (callback == nullptr) {
+                    sei();
+                    return;
+                }
+
+                uint64_t time_us = 0;
+
+                switch (unit) {
+                    case Sec:
+                        time_us = (uint64_t)duration * 1000000UL;
+                        break;
+                    case Hour:
+                        time_us = (uint64_t)duration * 3600UL * 1000000UL;
+                        break;
+                    default:
+                        time_us = duration;
+                        break;
+                }
+
+                uint16_t prescaler_val = 1;
+                uint16_t prescaler_bits = (1 << CS10);
+                uint64_t ocr = 0;
+
+                if (time_us < (uint64_t)65536 * 1 * 1000000UL / F_CPU) {
+                    prescaler_val = 1;
+                    prescaler_bits = (1 << CS10);
+                } else if (time_us < (uint64_t)65536 * 8 * 1000000UL / F_CPU) {
+                    prescaler_val = 8;
+                    prescaler_bits = (1 << CS11);
+                } else if (time_us < (uint64_t)65536 * 64 * 1000000UL / F_CPU) {
+                    prescaler_val = 64;
+                    prescaler_bits = (1 << CS11) | (1 << CS10);
+                } else if (time_us < (uint64_t)65536 * 256 * 1000000UL / F_CPU) {
+                    prescaler_val = 256;
+                    prescaler_bits = (1 << CS12);
+                } else {
+                    prescaler_val = 1024;
+                    prescaler_bits = (1 << CS12) | (1 << CS10);
+                }
+
+                ocr = (time_us * F_CPU) / (prescaler_val * 1000000UL);
+                if (ocr > 65535) {
+                    OCR1A = 65535;
+                } else {
+                    OCR1A = ocr - 1;
+                }
+
+                TCCR1A = 0;
+                TCCR1B = (1 << WGM12);
+                TIMSK1 = (1 << OCIE1A);
+                TCCR1B |= prescaler_bits;
+
+                sei();
             }
         };
 
-        struct RegC {
-            inline static volatile uint8_t& ddr() {
-                return DDRC;
-            }
+        void (*Timer1::callback)() = nullptr;
 
-            inline static volatile uint8_t& port() {
-                return PORTC;
+        ISR(TIMER1_COMPA_vect) {
+            if (Timer1::callback) {
+                Timer1::callback();
             }
-
-            inline static volatile uint8_t& pin() {
-                return PINC;
-            }
-        };
-
-        struct RegD {
-            inline static volatile uint8_t& ddr() {
-                return DDRD;
-            }
-
-            inline static volatile uint8_t& port() {
-                return PORTD;
-            }
-
-            inline static volatile uint8_t& pin() {
-                return PIND;
-            }
-        };
+        }
     }
 
-    template<typename R, uint8_t BIT>
-    struct PinImpl {
-        inline static void into_output() {
-            R::ddr() |= (1<<BIT);
+    namespace io {
+        namespace registers {
+            template<typename R>
+            struct IRegister {
+                inline static void set_port(uint8_t value) {
+                    R::port() = value;
+                }
+
+                inline static uint8_t get_port() {
+                    return R::port();
+                }
+
+                inline static void set_ddr(uint8_t value) {
+                    R::ddr() = value;
+                }
+
+                inline static uint8_t get_ddr() {
+                    return R::ddr();
+                }
+
+                inline static uint8_t get_pin() {
+                    return R::pin();
+                }
+
+                inline static void into_output() {
+                    R::ddr() = 0xff;
+                }
+
+                inline static void into_input() {
+                    R::ddr() = 0x00;
+                }
+
+                inline static void into_input_floating() {
+                    R::ddr() = 0x00;
+                    R::port() = 0x00;
+                }
+
+                inline static void into_input_pullup() {
+                    R::ddr() = 0x00;
+                    R::port() = 0xff;
+                }
+            };
+
+            struct B: IRegister<B> {
+                inline static volatile uint8_t& ddr() {
+                    return DDRB;
+                }
+
+                inline static volatile uint8_t& port() {
+                    return PORTB;
+                }
+
+                inline static volatile uint8_t& pin() {
+                    return PINB;
+                }
+            };
+
+            struct C: IRegister<C> {
+                inline static volatile uint8_t& ddr() {
+                    return DDRC;
+                }
+
+                inline static volatile uint8_t& port() {
+                    return PORTC;
+                }
+
+                inline static volatile uint8_t& pin() {
+                    return PINC;
+                }
+            };
+
+            struct D: IRegister<D> {
+                inline static volatile uint8_t& ddr() {
+                    return DDRD;
+                }
+
+                inline static volatile uint8_t& port() {
+                    return PORTD;
+                }
+
+                inline static volatile uint8_t& pin() {
+                    return PIND;
+                }
+            };
         }
 
-        inline static void into_input() {
-            R::ddr() &= ~(1<<BIT);
-        }
+        template<typename R, uint8_t BIT>
+        struct PinImpl {
+            inline static void into_output() {
+                R::ddr() |= (1<<BIT);
+            }
 
-        inline static void into_input_floating() {
-            R::ddr() &= ~(1<<BIT);
-            R::port() &= ~(1<<BIT);
-        }
+            inline static void into_input() {
+                R::ddr() &= ~(1<<BIT);
+            }
 
-        inline static void into_input_pullup() {
-            R::ddr() &= ~(1<<BIT);
-            R::port() |= (1<<BIT);
-        }
-
-        inline static void set_high() {
-            R::port() |= (1<<BIT);
-        }
-
-        inline static void set_low() {
-            R::port() &= ~(1<<BIT);
-        }
-
-        inline static void set(uint8_t value) {
-            if (value) {
-                R::port() |= (1<<BIT);
-            } else {
+            inline static void into_input_floating() {
+                R::ddr() &= ~(1<<BIT);
                 R::port() &= ~(1<<BIT);
             }
-        }
 
-        inline static uint8_t get() {
-            return (R::pin() & (1<<BIT)) ? 1 : 0;
-        }
-    };
+            inline static void into_input_pullup() {
+                R::ddr() &= ~(1<<BIT);
+                R::port() |= (1<<BIT);
+            }
 
-    using d0 = PinImpl<regs::RegD, 0>;
-    using d1 = PinImpl<regs::RegD, 1>;
-    using d2 = PinImpl<regs::RegD, 2>;
-    using d3 = PinImpl<regs::RegD, 3>;
-    using d4 = PinImpl<regs::RegD, 4>;
-    using d5 = PinImpl<regs::RegD, 5>;
-    using d6 = PinImpl<regs::RegD, 6>;
-    using d7 = PinImpl<regs::RegD, 7>;
-    using d8 = PinImpl<regs::RegB, 0>;
-    using d9 = PinImpl<regs::RegB, 1>;
-    using d10 = PinImpl<regs::RegB, 2>;
-    using d11 = PinImpl<regs::RegB, 3>;
-    using d12 = PinImpl<regs::RegB, 4>;
-    using d13 = PinImpl<regs::RegB, 5>;
-    using a0 = PinImpl<regs::RegC, 0>;
-    using a1 = PinImpl<regs::RegC, 1>;
-    using a2 = PinImpl<regs::RegC, 2>;
-    using a3 = PinImpl<regs::RegC, 3>;
-    using a4 = PinImpl<regs::RegC, 4>;
-    using a5 = PinImpl<regs::RegC, 5>;
-    using led = d13;
+            inline static void set_high() {
+                R::port() |= (1<<BIT);
+            }
+
+            inline static void set_low() {
+                R::port() &= ~(1<<BIT);
+            }
+
+            inline static void set(uint8_t value) {
+                if (value) {
+                    R::port() |= (1<<BIT);
+                } else {
+                    R::port() &= ~(1<<BIT);
+                }
+            }
+
+            inline static void toggle() {
+                R::port() ^= (1<<BIT);
+            }
+
+            inline static uint8_t get() {
+                return (R::pin() & (1<<BIT)) ? 1 : 0;
+            }
+        };
+
+        using d0 = PinImpl<registers::D, 0>;
+        using d1 = PinImpl<registers::D, 1>;
+        using d2 = PinImpl<registers::D, 2>;
+        using d3 = PinImpl<registers::D, 3>;
+        using d4 = PinImpl<registers::D, 4>;
+        using d5 = PinImpl<registers::D, 5>;
+        using d6 = PinImpl<registers::D, 6>;
+        using d7 = PinImpl<registers::D, 7>;
+        using d8 = PinImpl<registers::B, 0>;
+        using d9 = PinImpl<registers::B, 1>;
+        using d10 = PinImpl<registers::B, 2>;
+        using d11 = PinImpl<registers::B, 3>;
+        using d12 = PinImpl<registers::B, 4>;
+        using d13 = PinImpl<registers::B, 5>;
+        using a0 = PinImpl<registers::C, 0>;
+        using a1 = PinImpl<registers::C, 1>;
+        using a2 = PinImpl<registers::C, 2>;
+        using a3 = PinImpl<registers::C, 3>;
+        using a4 = PinImpl<registers::C, 4>;
+        using a5 = PinImpl<registers::C, 5>;
+        using led = d13;
+    }
 }
--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,32 +1,24 @@
 #define F_CPU 16000000UL
 #include "avr_io.hpp"
 
+namespace io = atmega_328p::io;
+namespace timers = atmega_328p::timers;
+
 int main() {
+    io::registers::B::into_input_pullup();
     io::led::into_output();
 
-    while (1) {
-        io::led::set_high();
-        _delay_ms(500);
+    timers::Timer1::init(
+        timers::TimeUnit::Sec,
+        2,
+        [](){
+            io::led::toggle();
+        }
+    );
 
-        io::led::set_low();
+    while (1) {
         _delay_ms(500);
     }
 
     return 0;
 }
-
-/*
-int main() {
-    DDRB |= (1<<5);
-
-    while (1) {
-         PORTB |= (1<<5);
-        _delay_ms(500);
-
-         PORTB &= ~(1<<5);
-        _delay_ms(500);
-    }
-
-    return 0;
-}
-*/
